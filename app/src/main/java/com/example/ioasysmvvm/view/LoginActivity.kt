f70e53e763f107756e6889a35152d58d07f3eb92
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-16 22:02:05 -0300

    create ImageViewExtensions and ContextExtensions

--- app/src/main/java/com/example/ioasysmvvm/view/LoginActivity.kt
+++ app/src/main/java/com/example/ioasysmvvm/view/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.example.ioasysmvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.ioasysmvvm.R
+import com.example.ioasysmvvm.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginViewModel: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginViewModel = ViewModelProvider.NewInstanceFactory().create(LoginViewModel::class.java)
     }
 }
--- app/src/main/java/com/example/ioasysmvvm/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/ioasysmvvm/viewmodel/LoginViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.ioasysmvvm.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.ioasysmvvm.model.api.Api
+import com.example.ioasysmvvm.model.api.DataService
+import com.example.ioasysmvvm.model.domains.user.UserRequest
+import okhttp3.ResponseBody
+import retrofit2.Call
+
+
+class LoginViewModel : ViewModel() {
+    fun initLoginViewModel(userRequest: UserRequest) {
+        val dataService: DataService = Api.setupRetrofit().create(DataService::class.java)
+        val call: Call<ResponseBody> = dataService.recoverVerifyLogin(userRequest)
+        doLogin(call)
+    }
+    private fun doLogin(call: Call<ResponseBody>) {
+
+    }
 }
