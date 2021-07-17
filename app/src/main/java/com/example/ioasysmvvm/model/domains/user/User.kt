649dc4155f26eb59e93be3bc210ac65259865b76
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-16 21:22:12 -0300

    create StringExtensions and add fun isValidEmail and isValidPassword in User

--- app/src/main/java/com/example/ioasysmvvm/model/domains/user/User.kt
+++ app/src/main/java/com/example/ioasysmvvm/model/domains/user/User.kt
@@ -1,3 +1,23 @@
 package com.example.ioasysmvvm.model.domains.user
 
-data class User(val email: String, val password: String)
+import com.example.ioasysmvvm.model.extensions.isValidEmail
+
+data class User(val email: String, val password: String){
+    fun isValidEmail(): EmailStatus {
+        return when {
+            email.isValidEmail() -> {
+                EmailStatus.VALID
+            }
+            email.isEmpty() -> {
+                EmailStatus.EMPTY
+            }
+            else -> {
+                EmailStatus.INVALID
+            }
+        }
+    }
+
+    fun isValidPassword(): Boolean {
+        return password.isNotEmpty()
+    }
+}
