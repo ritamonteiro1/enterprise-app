2dd46c68e6943cbf0f27b744d51166b91cc6805e
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-22 11:59:49 -0300

    add fun findViewsById in MainActivity.kt

--- app/src/main/java/com/example/ioasysmvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/ioasysmvvm/view/MainActivity.kt
@@ -1,10 +1,31 @@
 package com.example.ioasysmvvm.view
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ioasysmvvm.R
+import com.example.ioasysmvvm.model.extensions.createLoadingDialog
 
 class MainActivity : AppCompatActivity() {
+    private var mainInformationTextView: TextView? = null
+    private var mainToolBar: Toolbar? = null
+    private var mainRecyclerView: RecyclerView? = null
+    private var loadingDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadingDialog = createLoadingDialog()
+        findViewsById()
+
+    }
+
+    private fun findViewsById() {
+        mainInformationTextView = findViewById(R.id.mainInformationTextView)
+        mainToolBar = findViewById(R.id.mainToolBar)
+        mainRecyclerView = findViewById(R.id.mainRecyclerView)
     }
 }
