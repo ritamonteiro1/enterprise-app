7a4cfd0f28a86f389e40d6569e454aa84a3762a2
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-22 12:02:56 -0300

    add fun setupToolBar in MainActivity.kt

--- app/src/main/java/com/example/ioasysmvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/ioasysmvvm/view/MainActivity.kt
@@ -20,8 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         loadingDialog = createLoadingDialog()
         findViewsById()
-
+        setupObservers()
+        setupToolBar()
     }
+
+    private fun setupObservers() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupToolBar() {
+        setSupportActionBar(mainToolBar)
+        supportActionBar?.setLogo(R.drawable.img_logo_nav)
+        supportActionBar?.setDisplayShowHomeEnabled(false)
+        supportActionBar?.setDisplayUseLogoEnabled(true)    }
 
     private fun findViewsById() {
         mainInformationTextView = findViewById(R.id.mainInformationTextView)
