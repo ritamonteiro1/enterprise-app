0f77955ae67e4986a1127319aaaab9c3348a4205
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-22 12:07:25 -0300

    change in EnterpriseService.kt

--- app/src/main/java/com/example/ioasysmvvm/model/api/EnterpriseService.kt
+++ app/src/main/java/com/example/ioasysmvvm/model/api/EnterpriseService.kt
@@ -2,7 +2,6 @@ package com.example.ioasysmvvm.model.api
 
 import com.example.ioasysmvvm.model.constants.Constants
 import com.example.ioasysmvvm.model.domains.enterprise.EnterpriseListResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -15,5 +14,5 @@ interface EnterpriseService {
         @Header("access-token") accessToken: String,
         @Header("client") client: String,
         @Header("uid") uid: String
-    ): Call<EnterpriseListResponse>
+    ): EnterpriseListResponse
 }
