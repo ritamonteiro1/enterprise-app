f0275ae3cb53f6ba08b6e4953ba3a683fa0299f0
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-28 09:04:27 -0300

    remove fun findViewById and add viewBinding in LoginActivity.kt

--- app/src/main/java/com/example/ioasysmvvm/view/LoginActivity.kt
+++ app/src/main/java/com/example/ioasysmvvm/view/LoginActivity.kt
@@ -4,37 +4,31 @@ import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import com.example.ioasysmvvm.R
+import com.example.ioasysmvvm.databinding.ActivityLoginBinding
 import com.example.ioasysmvvm.model.constants.Constants
 import com.example.ioasysmvvm.model.domains.user.EmailStatus
 import com.example.ioasysmvvm.model.domains.user.User
 import com.example.ioasysmvvm.model.extensions.createLoadingDialog
 import com.example.ioasysmvvm.viewmodel.LoginViewModel
-import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
     private val loginViewModel: LoginViewModel by viewModel()
-    private var loginEmailTextInputLayout: TextInputLayout? = null
-    private var loginEmailEditText: EditText? = null
-    private var loginPasswordTextInputLayout: TextInputLayout? = null
-    private var loginPasswordEditText: EditText? = null
-    private var loginButton: Button? = null
+    private var loginViewBinding: ActivityLoginBinding? = null
     private var loadingDialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        loadingDialog = createLoadingDialog()
-        findViewsById()
+        loginViewBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginViewBinding?.root)
+        loadingDialog = createLoadingDialog ()
         setupObservers()
         setupLoginButton()
     }
 
     private fun setupLoginButton() {
-        loginButton?.setOnClickListener {
+       loginViewBinding?.loginButton?.setOnClickListener {
             val user = setupUser()
             loginViewModel.doLogin(user)
         }
@@ -66,36 +60,28 @@ class LoginActivity : AppCompatActivity() {
 
     private fun treatInvalidPassword(isValidPassword: Boolean) {
         if (isValidPassword) {
-            loginPasswordTextInputLayout?.error = Constants.EMPTY
+            loginViewBinding?.loginPasswordTextInputLayout?.error = Constants.EMPTY
         } else {
-            loginPasswordTextInputLayout?.error = getString(R.string.fill_the_field)
+            loginViewBinding?.loginPasswordTextInputLayout?.error = getString(R.string.fill_the_field)
         }
     }
 
     private fun treatInvalidEmail(isValidEmail: EmailStatus) {
         when (isValidEmail) {
             EmailStatus.VALID -> {
-                loginEmailTextInputLayout?.error = Constants.EMPTY
+                loginViewBinding?.loginEmailTextInputLayout?.error = Constants.EMPTY
             }
             EmailStatus.EMPTY -> {
-                loginEmailTextInputLayout?.error = getString(R.string.fill_the_field)
+                loginViewBinding?.loginEmailTextInputLayout?.error = getString(R.string.fill_the_field)
             }
             else -> {
-                loginEmailTextInputLayout?.error = getString(R.string.incorrect_email)
+                loginViewBinding?.loginEmailTextInputLayout?.error = getString(R.string.incorrect_email)
             }
         }
     }
 
     private fun setupUser() = User(
-        loginEmailEditText?.text?.toString().orEmpty(),
-        loginPasswordEditText?.text?.toString().orEmpty()
+        loginViewBinding?.loginEmailEditText?.text?.toString().orEmpty(),
+        loginViewBinding?.loginPasswordEditText?.text?.toString().orEmpty()
     )
-
-    private fun findViewsById() {
-        loginEmailEditText = findViewById(R.id.loginEmailEditText)
-        loginPasswordEditText = findViewById(R.id.loginPasswordEditText)
-        loginButton = findViewById(R.id.loginButton)
-        loginEmailTextInputLayout = findViewById(R.id.loginEmailTextInputLayout)
-        loginPasswordTextInputLayout = findViewById(R.id.loginPasswordTextInputLayout)
-    }
 }
