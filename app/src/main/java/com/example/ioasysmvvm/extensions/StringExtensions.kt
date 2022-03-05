a24d6f599513aa50d4dc9f9bfd8e8ee53707a30d
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-03-05 02:28:30 -0300

    delete StringExtensions.kt

--- app/src/main/java/com/example/ioasysmvvm/extensions/StringExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ioasysmvvm.extensions
-
-import android.util.Patterns
-
-fun String.isValidEmail(): Boolean {
-    return this.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
-}
