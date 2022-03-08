b40692c334383d69613a4e04281376ea95dd4b1a
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-03-08 13:50:24 -0300

    create test `GIVEN a call on getEnterpriseList WHEN request is fail THEN it should throw an Exception` in EnterpriseRepositoryImplTest.kt

--- app/src/test/java/com/example/ioasysmvvm/data/repository/enterprise/EnterpriseRepositoryImplTest.kt
+++ app/src/test/java/com/example/ioasysmvvm/data/repository/enterprise/EnterpriseRepositoryImplTest.kt
@@ -1,6 +1,7 @@
 package com.example.ioasysmvvm.data.repository.enterprise
 
 import com.example.ioasysmvvm.data.remote.enterprise.data_source.EnterpriseRemoteDataSource
+import com.example.ioasysmvvm.domain.model.result.Result
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -8,6 +9,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -26,7 +28,6 @@ class EnterpriseRepositoryImplTest {
         clearAllMocks()
     }
 
-    @ExperimentalCoroutinesApi
     @Test
     fun `GIVEN a call on getEnterpriseList THEN call enterpriseRemoteDataSource`() =
         runBlockingTest {
@@ -47,5 +48,31 @@ class EnterpriseRepositoryImplTest {
                     enterpriseName, accessToken, client, uid
                 )
             }
+        }
+
+    @Test
+    fun `GIVEN a call on getEnterpriseList WHEN request is fail THEN it should throw an Exception`() =
+        runBlockingTest {
+            val enterpriseName = "test"
+            val accessToken = "41213e12e"
+            val client = "teste@gmail.com"
+            val uid = "2132e12e12"
+
+            val expectedError: Result.Error = mockk(relaxed = true)
+
+
+            coEvery {
+                enterpriseRemoteDataSource.getEnterpriseList(any(), any(), any(), any())
+            } returns expectedError
+
+            enterpriseRepositoryImpl.getEnterpriseList(
+                enterpriseName, accessToken, client, uid
+            )
+            val result =
+                enterpriseRemoteDataSource.getEnterpriseList(
+                    enterpriseName, accessToken, client, uid
+                )
+
+            Assert.assertEquals(expectedError, result)
         }
 }
