ac5fca4b714cdb0f3fa0186bbd2b705ee04f6203
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-06-28 20:20:03 -0300

    create LoginActivityContent in LoginActivity.kt

--- featureauth/src/main/java/com/example/featureauth/presentation/LoginActivity.kt
+++ featureauth/src/main/java/com/example/featureauth/presentation/LoginActivity.kt
@@ -1,23 +1,68 @@
 package com.example.featureauth.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.featureauth.R
-import com.example.featureauth.databinding.ActivityLoginBinding
+import com.example.navigation.AuthBoundary
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
-    private lateinit var navHostFragment: NavHostFragment
+class LoginActivity : ComponentActivity() {
 
+    private val boundary: AuthBoundary by inject()
+    private val viewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            LoginActivityContent(viewModel, boundary)
+        }
+    }
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.loginFragmentContainer) as NavHostFragment
+    @Composable
+    fun LoginActivityContent(viewModel: LoginViewModel, boundary: AuthBoundary) {
+        val scrollState = rememberScrollState()
+        Scaffold(
+            backgroundColor = colorResource(R.color.beige)
+        ) {
+            Column(
+                modifier = Modifier.scrollable(
+                    state = scrollState,
+                    orientation = Orientation.Vertical
+                )
+            ) {
+                Spacer(modifier = Modifier.paddingFromBaseline(top = 62.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.img_home),
+                    contentDescription = stringResource(id = R.string.login_logo_content_description)
+                )
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    fun LoginActivityContentPreview() {
+        Image(
+            painter = painterResource(id = R.drawable.img_home),
+            contentDescription = ""
+        )
     }
 }
